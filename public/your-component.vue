cc58770a9020d272c2e7ebe1d7f7d8b24ce53037
yongju6 <jyongju928@gmail.com>
2022-07-03 11:43:58 +0900

    webapp component setting

---
+++ public/your-component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>
+      <button>{{message}}</button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "your-component",
+  data: {
+    message: "click this button"
+  }
+}
+</script>
+
+<style scoped>
+  span{
+    font-size: 1.2em;
+  }
+</style>
